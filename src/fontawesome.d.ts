b122d3b0e43910ac8351520a65363f1a1435636c
aradhya <aradhyay868@gmail.com>
2024-07-30 15:23:07 +0530

    first commit

--- src/fontawesome.d.ts
+++
@@ -1,5 +0,0 @@
-// fontawesome.d.ts
-import { DefineComponent } from 'vue';
-
-declare const FontAwesome: DefineComponent<{}, {}, any>;
-export default FontAwesome;
--- src/fontawesome.js
+++
@@ -1,7 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(faBars, faTimes);
-
-export { FontAwesomeIcon };
