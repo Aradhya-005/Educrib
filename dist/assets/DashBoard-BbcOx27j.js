64e46c4665dc8803a89fffef4631f07f7432eae5
aradhya <aradhyay868@gmail.com>
2024-10-04 11:44:54 +0530

    fixing the deployment error

--- dist/assets/DashBoard-BbcOx27j.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as a,c,p as _,i as d,b as e}from"./index-BUUeTdJ4.js";const n={},s=o=>(_("data-v-596fbf30"),o=o(),d(),o),p=s(()=>e("h1",null,"helloooo :)",-1)),r=s(()=>e("h5",null,"I am DashBoard",-1)),l=[p,r];function h(o,f,i,u,m,B){return a(),c("div",null,l)}const $=t(n,[["render",h],["__scopeId","data-v-596fbf30"]]);export{$ as default};
